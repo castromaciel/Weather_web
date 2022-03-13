09d804061d33d388c228aebc90295dfdfd607012
Castro Maciel <castromaciel00@gmail.com>
2022-03-13 17:39:36 -0300

    Passing props from boxresults to citiescard

--- src/components/Search/components/CitiesCard/CitiesCard.tsx
+++ src/components/Search/components/CitiesCard/CitiesCard.tsx
@@ -1,13 +1,19 @@
+import { CityFoundCopy } from '../../interfaces/interfaces'
 import './cities-card.css'
 
-export const CitiesCard = () => {
+export const CitiesCard = (props:CityFoundCopy) => {
+
+  console.log(props)
+
   return (
-    <div className='shadow cities-card d-flex align-items-center justify-content-between text-white'>
-      <img className="city-weather-img PS-" src='./favicon.ico' alt='favicon'/>
 
-      <h3>City, State, Country</h3>
-
-      <h3>23°C</h3>
+    <div key={props.id} className='shadow cities-card d-flex align-items-center text-white'>
+      <div className='d-flex align-items-center'>
+        <img className="city-weather-img PS-" src='./favicon.ico' alt='favicon' />
+        <h2 className='m-0 ps-3'>23°C</h2>
+      </div>
+      <h3 className='ps-1 ms-1 ms-sm-3 ps-sm-3 ms-md-5 ps-md-5 m-0'>{props.name}</h3>
     </div>
+
   )
 }
--- src/components/Search/components/SearchBox/SearchBoxResults.tsx
+++ src/components/Search/components/SearchBox/SearchBoxResults.tsx
@@ -1,6 +1,7 @@
 import './search-box-results.css'
 import { CitiesCard } from '../CitiesCard/CitiesCard';
 import { useSearch } from '../../hooks/useSearch';
+import { CityFoundCopy } from '../../interfaces/interfaces';
 
 function SearchBoxResults() {
 
@@ -8,9 +9,8 @@ function SearchBoxResults() {
 
   return (
       <div className={`col-10 col-md-8 col-lg-6 show-results shadow`}>
-        {location.map( () => <CitiesCard /> )}        
+        {location.map( (l:CityFoundCopy) => <CitiesCard {...l} /> )}        
       </div>
   )
 }
-
 export default SearchBoxResults

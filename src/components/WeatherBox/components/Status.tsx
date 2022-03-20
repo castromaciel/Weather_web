d1036d88765fdf29ab6d27e37dc73a0fcb0f0d50
Castro Maciel <castromaciel00@gmail.com>
2022-03-20 18:52:34 -0300

    adding styles

--- src/components/WeatherBox/components/Status.tsx
+++ src/components/WeatherBox/components/Status.tsx
@@ -8,7 +8,7 @@ export const Status = () => {
   const index = weather_array.findIndex( i => i === weather.description)
 
   return (
-    <div className={`d-flex flex-column align-items-center`}>
+    <div className={`py-4 d-flex flex-column align-items-center`}>
       <h3>{weather.name}</h3>
       
       <img 
@@ -22,9 +22,9 @@ export const Status = () => {
 
       <h3>{weather.temp}°C</h3>
       <h3 className='text-capitalize'>{weather.description}</h3>
-      <div className={`d-flex`}>
-        <h4 className="px-1 mx-3">Min: {weather.min}°C</h4>
-        <h4 className="px-1 mx-3">Max: {weather.max}°C</h4>
+      <div className={`d-flex justify-content-around w-100`}>
+        <h4 className=" ">Min: {weather.min}°C</h4>
+        <h4 className=" ">Max: {weather.max}°C</h4>
       </div>
     </div>
   )
--- src/components/WeatherBox/components/WeatherCard.tsx
+++ src/components/WeatherBox/components/WeatherCard.tsx
@@ -4,7 +4,7 @@ import { Status } from './Status';
 export const WeatherCard = () => {
   return (
     <div className='d-flex alig-items-center justify-content-center'>
-      <div className={`bg-light text-dark mt-5 col-10 col-md-8 col-lg-6 ${styles.card__weather_box}`}>
+      <div className={`bg-light text-dark my-3 col-10 col-md-8 col-lg-6 ${styles.card__weather_box}`}>
         <Status />
       </div>
     </div>
